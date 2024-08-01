a6aa448f4b4947f60fe0a0f601556dd0b4d26398
mrycky23 <rycky_jvm@outlook.com>
2024-08-01 17:02:19 -0500

    revision

--- tarea.php
+++ tarea.php
@@ -1,20 +1,20 @@
 <?php
 
-// a. Declaración de Variables:
+//Declaración de Variables:
 $edad = 25;
 $altura = 1.75;
 $nombre = "Juan Pérez";
 $esEstudiante = false;
 $frutas = array("mango", "fresa", "kiwi", "papaya", "sandía");
 
-// b. Operaciones Aritméticas:
+//Operaciones Aritméticas:
 $diferencia = $edad - 5;
 $division = $altura / 2;
 
 echo "La diferencia entre $edad y 5 es: $diferencia<br>";
 echo "La mitad de la altura ($altura) es: $division<br>";
 
-// c. Manipulación de Cadenas:
+//Cadenas:
 $apellido = "García";
 $nombreCompleto = $nombre . " " . $apellido;
 $longitudNombre = strlen($nombreCompleto);
@@ -22,13 +22,13 @@ $longitudNombre = strlen($nombreCompleto);
 echo "Nombre completo: $nombreCompleto<br>";
 echo "La longitud del nombre completo es: $longitudNombre caracteres<br>";
 
-// d. Uso de Condicionales:
+//Condicionales:
 if ($esEstudiante) {
     echo "El usuario es estudiante<br>";
 } else {
     echo "El usuario no es estudiante<br>";
 }
 
-// e. Creación de un Array:
+//Array:
 echo "El cuarto elemento del arreglo es: " . $frutas[3] . "<br>";
 ?>
